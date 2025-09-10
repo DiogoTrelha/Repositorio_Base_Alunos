9a09c85214dfb1d385f7000709e48a092b38f627
DiogoTrelha <diogotrelha2134@gmail.com>
2025-09-10 16:14:00 -0300

    Add files via upload

---
+++ Modulo 1/Listas/01lista.py
@@ -0,0 +1,8 @@
+# crie uma lista vazia chamada fruta.
+# peça para o usuario digitar o nome de uma fruta e use .append() para adiciona-las a listas.
+# mostre a lista no final
+frutas = [] # criamos a lista vazia 
+fruta = input("digite uma fruta: ")# solicitamos ao usuario a informaçao
+frutas.append(fruta) # adicionamos a fruta informada na lista
+
+print(frutas) # apresentamos a lista
---
+++ Modulo 1/Listas/03lista.py
@@ -0,0 +1,9 @@
+#dada a lista nomes = ["ana", "carlos", "maria"],
+#use .insert()
+#para adicionar o nome "joao" na
+# segunda posiçao ( indice 1)
+
+nomes = ["ana", "carlos", "maria"]
+nomes.insert(1, "joao") # adicionamos um elemento em uma posiçao especifica
+
+print(nomes)
